8d00bcc39dbe26b064dc517a0030b07088acda84
Roma Bhattacharjee <roma0615@gmail.com>
2020-04-24 23:56:27 -0500

    Export databaseRef, not todosRef

--- Groshary/src/firebase.tsx
+++ Groshary/src/firebase.tsx
@@ -11,5 +11,4 @@ const config = {
     measurementId: "G-TJ6WB8T75F"
 };
 firebase.initializeApp(config);
-const databaseRef = firebase.database().ref();
-export const todosRef = databaseRef.child("todos");
+export const databaseRef = firebase.database().ref();
