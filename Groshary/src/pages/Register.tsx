0b03935ef4dffa74430cc5d8f2226308a963bde1
Jeffrey Huang <jeffreyhuang165@gmail.com>
2020-04-25 12:23:00 -0500

    new segue buttons

--- Groshary/src/pages/Register.tsx
+++ Groshary/src/pages/Register.tsx
@@ -59,6 +59,7 @@ const Register: React.FC = () => {
          	<IonButton expand="block" onClick={ () => register() }>
            Register
            </IonButton>
+           <IonButton color="warning" fill="outline" href="/login">Already have an account? Login here</IonButton>
         </IonList>
       </IonContent>
     </IonPage>
